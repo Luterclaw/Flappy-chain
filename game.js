bc87a9074a57a18cb1f8ad15cbc04df086c6b138
Luterclaw <lucaspatricio275@gmail.com>
2025-05-30 12:28:29 -0300

    Update game.js

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ let OBSTACLE_WIDTH = 60;
 let characterImg = new Image();
 characterImg.src = "character_gold_centered.webp";
 let columnImg = new Image();
-columnImg.src = "chain_segment_gild.webp";
+columnImg.src = "chain_segment_gold.webp";
 let scoreDisplay = document.getElementById("score");
 
 function drawBird() {
