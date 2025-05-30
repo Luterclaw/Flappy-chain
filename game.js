ffcbd04fc9b5b549b4a9782e111fbc5176f08afb
Luterclaw <lucaspatricio275@gmail.com>
2025-05-30 12:40:17 -0300

    Update game.js

--- game.js
+++ game.js
@@ -51,10 +51,12 @@ function drawPipes() {
 
 function updatePipes() {
   if (frame % 90 === 0) {
-    let top = Math.random() * 180 + 20;
-    let gap = 100;
-    pipes.push({ x: canvas.width, top: top, bottom: top + gap, scored: false });
-  }
+    let gap = 130;
+let centerY = Math.random() * (canvas.height - gap - 100) + 50; // margen de seguridad
+let top = centerY - gap / 2;
+let bottom = centerY + gap / 2;
+pipes.push({ x: canvas.width, top: top, bottom: bottom, scored: false });
+
   pipes.forEach(pipe => {
     pipe.x -= 2;
     if (!pipe.scored && pipe.x + OBSTACLE_WIDTH < bird.x) {
